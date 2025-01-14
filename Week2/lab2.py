10b2541b3b3f90242889830eece2870cab3b1536
Uzma Shaikh <125516304+uzmashxxxikh@users.noreply.github.com>
2025-01-14 12:24:31 -0500

    Update lab2.py

--- Week2/lab2.py
+++ Week2/lab2.py
@@ -1,51 +1,35 @@
 import random
 
-
-# def get_valid_int_input(prompt):
-#     while True:
-#         try:
-#             return int(input(prompt))
-#         except ValueError:
-#             print("Error: Please enter a valid integer!")
-#             continue
-# try:
-#     elements_selected = get_valid_int_input("Enter the index of the element you like: ")
-#     # Roll dice
-#     elementRoll = random.randint(1, 6)
-#     totalNum = elements_selected + elementRoll
-
-
 weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
-print("Choose Your Weapons: ", weapons)
 
-def get_wepon_input(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("Error: Please enter a valid integer!")
-            continue
+weaponRoll = random.randint(1, 6)
+print("Weapon Roll: ", weaponRoll)
 
-try:
-    wepon_selected = get_wepon_input("Enter the index of the weapon you like: ")
-    combatStrength = wepon_selected
-    weaponRoll = random.randint(1, 6)
-    totalNum = weaponRoll + combatStrength
+combat_strength = weaponRoll
+
+if 0 <= weaponRoll - 1 < len(weapons):
+    hero_weapon = weapons[weaponRoll - 1]
+else:
+    raise IndexError("Weapon roll is out of valid range")
+    print(f"Hero's weapon: {hero_weapon}")
+
 
     weaponRoll = int(input("Enter a weapon roll (1 to 6): "))
 
+try:
     if weaponRoll <= 2:
         print("You rolled a weak weapon, friend.")
     elif weaponRoll <= 4:
         print("Your weapon is meh.")
-    elif weaponRoll <= 6:
-        print("Nice weapon, friend!")
     else:
-        print("Error: Weapon roll must be between 1 and 6.")
+        print("Nice weapon, friend!")
 
-except IndexError:
-    print("Error: Invalid element index!")  # This exception is not relevant for this context.
+    if hero_weapon != "Fist":
+        print("Thank goodness you didn't roll the Fist...")
+
 except ValueError:
-    print("Error: Please enter a valid number.")
+    print("Error: Invalid input. Please enter a valid integer.")
+except IndexError:
+    print("Error: Invalid roll. The roll must be between 1 and 6.")
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
