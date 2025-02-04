83073f848c8bc1cef27c20e265b4093c23de083b
Uzma Shaikh <125516304+uzmashxxxikh@users.noreply.github.com>
2025-02-04 11:10:54 -0500

    Update functions_lab05.py

--- Week5/Week05/functions_lab05.py
+++ Week5/Week05/functions_lab05.py
@@ -1,5 +1,11 @@
 # functions_lab05.py
+import random
 
 
-
-# Question 3:
+# Question 3
+def collect_loot(loot_options, belt):
+    print("Random loot process:")
+    loot_roll = random.choice(range(1, len(loot_options) + 1))
+    loot = loot_options.pop(loot_roll - 1)
+    print("    |    Your belt: ", belt)
+    return loot_options, belt
